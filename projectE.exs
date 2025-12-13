2fbdd4f4c77b8c55809beb4d03c2ccad8decfe2e
benjaminyeom <benyeom042@gmail.com>
2025-12-13 10:54:16 -0800

    BCore test cases

--- projectE.exs
+++ projectE.exs
@@ -1,4 +1,4 @@
-defmodule BCoreModule do 
+defmodule BCoreModule do
     @type BCore ::
         {:numCE, number()} |
         {:biopCE, atom(), BCore(), BCore()} |
@@ -10,3 +10,51 @@ defmodule BCoreModule do
         {:appCE, BCore(), list(BCore())}
 end
 
+ExUnit.start()
+
+defmodule BCoreTest do
+  use ExUnit.Case
+  alias BCoreModule, as: B
+
+  test "numCE" do
+    assert {:numCE, 5} ==
+           {:numCE, 5}
+  end
+
+  test "biopCE" do
+    assert {:biopCE, :+, {:numCE, 2}, {:numCE, 3}} ==
+           {:biopCE, :+, {:numCE, 2}, {:numCE, 3}}
+  end
+
+  test "boolCE" do
+    assert {:boolCE, true} ==
+           {:boolCE, true}
+  end
+
+  test "condCE" do
+    assert {:condCE,
+            {:boolCE, true},
+            {:numCE, 1},
+            {:numCE, 2}} ==
+           {:condCE,
+            {:boolCE, true},
+            {:numCE, 1},
+            {:numCE, 2}}
+  end
+
+  test "varCE" do
+    assert {:varCE, :x} ==
+           {:varCE, :x}
+  end
+
+  test "letCE" do
+    assert {:letCE,
+            :x,
+            {:numCE, 1},
+            {:varCE, :x}} ==
+           {:letCE,
+            :x,
+            {:numCE, 1},
+            {:varCE, :x}}
+  end
+end
